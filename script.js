ab034d74baa3dcf8b46c4656601459aeedd83bb8
LinaEngman <150683543+LinaEngman@users.noreply.github.com>
2023-11-24 14:12:11 +0100

    update js

---
+++ script.js
@@ -0,0 +1,9 @@
+const checkbox = document.getElementById('divStyle');
+
+console.log(checkbox);
+
+const textfields = document.querySelectorAll('.textfield');
+
+console.log(textfields);
+
+const button = document.getElementsByTagName('button');
